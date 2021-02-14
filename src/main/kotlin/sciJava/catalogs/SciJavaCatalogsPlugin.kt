a551f4d9cf5542415918c0039daafa7a0aa94331
Giuseppe Barbieri <elect86@gmail.com>
2021-02-14 16:48:01 +0100

    imglib2

--- src/main/kotlin/sciJava/catalogs/SciJavaCatalogsPlugin.kt
+++ src/main/kotlin/sciJava/catalogs/SciJavaCatalogsPlugin.kt
@@ -127,40 +127,7 @@ class SciJavaCatalogsPlugin : Plugin<Settings> {
             //            <!-- Op Finder - https://github.com/imagej/op-finder -->
             //            <op-finder.version>0.1.4</op-finder.version>
             /*
-                                    <!-- ImgLib2 - https://github.com/imglib -->
 
-                                    <!-- ImgLib2 - https://github.com/imglib/imglib2 -->
-                                    <imglib2.version>5.11.1</imglib2.version>
-
-                                    <!-- ImgLib2 Algorithms - https://github.com/imglib/imglib2-algorithm -->
-                                    <imglib2-algorithm.version>0.11.2</imglib2-algorithm.version>
-
-                                    <!-- ImgLib2 Algorithms FFT - https://github.com/imglib/imglib2-algorithm-fft -->
-                                    <imglib2-algorithm-fft.version>0.2.0</imglib2-algorithm-fft.version>
-
-                                    <!-- ImgLib2 Algorithms GPL - https://github.com/imglib/imglib2-algorithm-gpl -->
-                                    <imglib2-algorithm-gpl.version>0.2.5</imglib2-algorithm-gpl.version>
-
-                                    <!-- ImgLib2 Cache - https://github.com/imglib/imglib2-cache -->
-                                    <imglib2-cache.version>1.0.0-beta-15</imglib2-cache.version>
-
-                                    <!-- ImgLib2 IJ - https://github.com/imglib/imglib2-ij -->
-                                    <imglib2-ij.version>2.0.0-beta-46</imglib2-ij.version>
-
-                                    <!-- ImgLib2 RealTransform - https://github.com/imglib/imglib2-realtransform -->
-                                    <imglib2-realtransform.version>3.1.0</imglib2-realtransform.version>
-
-                                    <!-- ImgLib2 ROI - https://github.com/imglib/imglib2-roi -->
-                                    <imglib2-roi.version>0.10.4</imglib2-roi.version>
-
-                                    <!-- ImgLib2 Scripting - https://github.com/imglib/imglib2-script -->
-                                    <imglib2-script.version>0.2.4</imglib2-script.version>
-
-                                    <!-- ImgLib2 UI - https://github.com/imglib/imglib2-ui -->
-                                    <imglib2-ui.version>2.0.1</imglib2-ui.version>
-
-                                    <!-- ImgLib2 Unsafe - https://github.com/imglib/imglib2-unsafe -->
-                                    <imglib2-unsafe.version>0.4.0</imglib2-unsafe.version>
 
                                     <!-- SCIFIO - https://github.com/scifio -->
 
