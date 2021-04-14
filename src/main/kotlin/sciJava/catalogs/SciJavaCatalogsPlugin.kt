3db4d94655b65a4ff5174e443280eeeccd977906
Giuseppe Barbieri <elect86@gmail.com>
2021-04-14 11:38:12 +0200

    :construction:  Bumop bigdataviewer-vistools to 1.0.0-beta-28

--- src/main/kotlin/sciJava/catalogs/SciJavaCatalogsPlugin.kt
+++ src/main/kotlin/sciJava/catalogs/SciJavaCatalogsPlugin.kt
@@ -322,7 +322,7 @@ class SciJavaCatalogsPlugin : Plugin<Settings> {
                 create("bigDataViewer").apply {
                     alias("core").to("sc.fiji:bigdataviewer-core:10.2.0")
                     alias("server").to("sc.fiji:bigdataviewer-server:3.0.0")
-                    alias("visTools").to("sc.fiji:bigdataviewer-vistools:1.0.0-beta-27")
+                    alias("visTools").to("sc.fiji:bigdataviewer-vistools:1.0.0-beta-28")
                     alias("fiji").to("sc.fiji:bigdataviewer_fiji:6.2.1")
                     alias("spimData").to("sc.fiji:spim_data:2.2.4")
                 }
